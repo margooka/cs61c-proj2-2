1cef878f900780547e7b643c74e810d89e36193e
margooka <oka@berkeley.edu>
2017-07-13 21:37:51 -0700

    started

--- linker-src/string.s
+++ linker-src/string.s
@@ -28,6 +28,17 @@ tab:	.asciiz "\t"
 #------------------------------------------------------------------------------
 strlen:
 	# YOUR CODE HERE
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+	addu $t0, $0, $0			#counter = 0
+	beq $a0, $0, strlenexit 		#if $a0 null exit
+	strlenloop: 
+	addiu $a0, $a0, 1			#a0 = pointer to next char
+	addiu $t0, $t0, 1			#counter +1
+	bne $a0, $0, strlenloop			#loop
+	strlenexit:
+	addu $v0, $t0, $0			#return $v0 = counter 
+	
 	jr $ra
 
 #------------------------------------------------------------------------------
@@ -42,6 +53,23 @@ strlen:
 #------------------------------------------------------------------------------
 strncpy:
 	# YOUR CODE HERE
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+	move $v0, $a0  			#$v0 = destination array $a0 -> $v0
+	beq $a1, $0, strncpyexit 	#if src str null
+	strncpyloop:
+	lb $t1, 0($a1) 			#t1 = char to copy 
+	sb $a0, 0($t1)			#put next src char in dest
+	addiu $a0, $a0, 1		#a0 = pointer to next dest char
+	addiu $a1, $a1, 1		#a1 = pointer to next src char
+	
+	addiu $a2, $a2, -1 		#counter -1
+	beq $a0, $0 strncpyexit 	#number of characters to copy > str length
+	beq $a2, $0 strncpyexit 	#copied  the $a2 number of characters
+	
+	strncpyexit:
+	
+	
 	jr $ra
 
 #------------------------------------------------------------------------------
@@ -58,6 +86,17 @@ strncpy:
 #------------------------------------------------------------------------------
 copy_of_str:
 	# YOUR CODE HERE
+	
+	jal strlen			#a0 = str input fo strlen
+	move $a2, $v0			#a2 = num char to copy for strncpy
+	sw $
+					#v0 = num bytes for syscall 9 to allocate
+					#a0 should = num bytes if syscall 
+	syscall 9			#v0 now contains address of allocated mem
+	move $a0, $v0			#a0 = dest array for strncpy
+					#a1 = source string
+					
+	jal strncpy
 	jr $ra
 
 ###############################################################################
